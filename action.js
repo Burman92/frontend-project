d82d6855157cc0b8e9cfce989630b2c32bc9876f
Burman Bedford <burman@Burmans-Air-414.lan>
2023-02-09 15:06:49 -0500

    I had to change the API I am using, because the original API was closed. So I used a random dad Joke API.

--- action.js
+++ action.js
@@ -1,11 +1,12 @@
 /* create a submit variable that can be used later
 create a variable that links to the ul element so that 
 we can and the dad joke as a list item*/
-let submit = $("#submit");
+let getJoke = $("#submit");
 let unorderedList = $(".list");
+// alert('If your Dad went to go get milk and never came back, dont worry this sight will provide you will all the Dad jokes that you need.')
 // let jokeList = unorderedList.append($("<li></li>"))
 // create a jquery that when you click the button will generate a random dad joke
-submit.click(function(){
+getJoke.click(function(){
 $.ajaxSetup({
     headers:{
         Accept: "application/json"
@@ -15,7 +16,7 @@ $.ajaxSetup({
 $.get("https://icanhazdadjoke.com/", function(data){
    //console logged this so that i can see that the information 
    // i am requesting is showing properly, i am getting the joke
-    console.log(data['joke'])
+    // console.log(data['joke'])
     //what i want to do now is add that text of the joke to the
     //unordered list in html
     //i will now create a variable that will hold the joke
@@ -28,6 +29,3 @@ $.get("https://icanhazdadjoke.com/", function(data){
 $("#clear").click(function(){
     unorderedList.empty()
 })
-// $('li').mouseenter(function(){
-//     $(li).css("font-weight", "Bold")
-// })
